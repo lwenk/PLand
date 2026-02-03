9d2ab9cda15677e5533527e4089241dff98bb91d
engsr6982 <pwxg286704@163.com>
2026-02-03 15:02:44 +0800

    refactor: implement Pimpl idiom for DevToolApp

--- src-devtool/DevToolApp.h
+++ src-devtool/DevToolApp.h
@@ -10,6 +10,9 @@
 namespace devtool {
 
 class DevToolApp final {
+    struct Impl;
+    std::unique_ptr<Impl> impl;
+
 public:
     explicit DevToolApp();
     ~DevToolApp();
@@ -28,39 +31,13 @@ public:
         requires std::derived_from<T, IMenu> && std::is_final_v<T>
     void registerMenu(Args&&... args) {
         auto ptr = std::make_unique<T>(std::forward<Args>(args)...);
-        menus_.emplace_back(std::move(ptr));
+        this->registerMenu(std::move(ptr));
     }
 
+    void registerMenu(std::unique_ptr<IMenu> menu);
+
     [[nodiscard]] static std::unique_ptr<DevToolApp> make();
-
-private:
-    void render();                     // 核心渲染 (renderErrors、renderMainMenuBar、postTick)
-    void renderErrors() const;         // 渲染错误信息
-    void renderMainMenuBar() const;    // 渲染主菜单栏 (renderAppOptionsMenu、renderFramerateInfo)
-    void renderFramerateInfo() const;  // 渲染帧率信息
-    void renderAppOptionsMenu() const; // 渲染应用选项菜单
-    void postTick() const;
-
-    void checkAndUpdateScale();
-
-    void initImGuiAndOpenGlWithGLFW();
-
-
-    GLFWwindow*                         glfwWindow_{nullptr};
-    std::thread                         renderThread_;
-    std::atomic<bool>                   renderThreadStopFlag_;
-    std::vector<std::string>            errors_;
-    std::vector<std::unique_ptr<IMenu>> menus_;
 };
-
-
-namespace internals {
-
-void handleGlfwError(int error, const char* description);
-
-void handleWindowClose(GLFWwindow* window);
-
-} // namespace internals
 
 
 } // namespace devtool
