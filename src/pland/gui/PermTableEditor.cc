98b8850051828da583d75ef1b2d35f1e358cb350
engsr6982 <pwxg286704@163.com>
2026-02-09 16:09:50 +0800

    feat(gui): restructure permission table editor with categorized forms

--- src/pland/gui/PermTableEditor.cc
+++ src/pland/gui/PermTableEditor.cc
@@ -1,3 +1,4 @@
+#include "utils/BackUtils.h"
 #include "PermTableEditor.h"
 
 #include "pland/land/repo/LandContext.h"
@@ -8,37 +9,125 @@
 namespace land {
 namespace gui {
 
-void PermTableEditor::sendTo(Player& player, LandPermTable const& table, Callback callback) {
+struct PermTableEditor::Impl : std::enable_shared_from_this<Impl> {
+    enum class EditType : uint8_t { Environment, Member, Guest };
+    Callback      mCallback;
+    LandPermTable mTable;
+
+    explicit Impl(Callback callback, LandPermTable const& table)
+    : mCallback(std::move(callback)),
+      mTable(std::move(table)) {}
+
+    void sendEdit(Player& player, EditType type) {
+        auto  localeCode = player.getLocaleCode();
+        auto& i18n       = ll::i18n::getInstance();
+
+        auto f = ll::form::CustomForm();
+        switch (type) {
+        case EditType::Environment:
+            f.setTitle("Perm - 全局权限管理"_trl(localeCode));
+            break;
+        case EditType::Member:
+            f.setTitle("Perm - 成员权限管理"_trl(localeCode));
+            break;
+        case EditType::Guest:
+            f.setTitle("Perm - 游客权限管理"_trl(localeCode));
+            break;
+        }
+
+        auto fields = nlohmann::json::object();
+        if (type == EditType::Environment) {
+            fields = ll::reflection::serialize<nlohmann::json>(mTable.environment).value();
+        } else {
+            fields = ll::reflection::serialize<nlohmann::json>(mTable.role).value();
+        }
+
+        for (auto& [k, v] : fields.items()) {
+            bool value = false;
+            switch (type) {
+            case EditType::Environment:
+                value = v.get<bool>();
+                break;
+            case EditType::Member:
+                static_assert(std::same_as<
+                              std::remove_const_t<decltype(std::declval<RolePerms::Entry>().member)>,
+                              bool>);
+                value = v["member"].get<bool>();
+                break;
+            case EditType::Guest:
+                static_assert(std::
+                                  same_as<std::remove_const_t<decltype(std::declval<RolePerms::Entry>().guest)>, bool>);
+                value = v["guest"].get<bool>();
+                break;
+            }
+            f.appendToggle(k, std::string{i18n.get(k, localeCode)}, value);
+        }
+
+        f.sendTo(
+            player,
+            [type, thiz = shared_from_this(), fields = std::move(fields)](
+                Player&                           player,
+                ll::form::CustomFormResult const& data,
+                ll::form::FormCancelReason
+            ) mutable {
+                if (!data) {
+                    return;
+                }
+
+                // 更新上一步的数据
+                for (auto& [k, v] : fields.items()) {
+                    bool newVal = std::get<uint64_t>(data->at(k));
+                    switch (type) {
+                    case EditType::Environment:
+                        v = newVal;
+                        break;
+                    case EditType::Member:
+                        v["member"] = newVal;
+                        break;
+                    case EditType::Guest:
+                        v["guest"] = newVal;
+                        break;
+                    }
+                }
+
+                // 反序列化回结构体
+                if (type == EditType::Environment) {
+                    ll::reflection::deserialize(thiz->mTable.environment, fields).value();
+                } else {
+                    ll::reflection::deserialize(thiz->mTable.role, fields).value();
+                }
+
+                thiz->mCallback(player, thiz->mTable);
+            }
+        );
+    }
+};
+
+void PermTableEditor::sendTo(
+    Player&                              player,
+    LandPermTable const&                 table,
+    Callback                             callback,
+    ll::form::SimpleForm::ButtonCallback backTo
+) {
     auto localeCode = player.getLocaleCode();
 
-    ll::form::CustomForm fm("[PLand] | 编辑权限"_trl(localeCode));
+    auto impl = std::make_shared<Impl>(std::move(callback), table);
 
-    auto& i18n = ll::i18n::getInstance();
-
-    auto json = json_util::struct2json(table);
-    for (auto& [k, v] : json.items()) {
-        fm.appendToggle(k, (std::string)i18n.get(k, localeCode), v);
+    auto f = ll::form::SimpleForm{};
+    f.setTitle("[PLand] - 权限管理"_trl(localeCode));
+    f.appendButton("环境权限"_trl(localeCode), "textures/ui/icon_recipe_nature", "path", [impl](Player& player) {
+        impl->sendEdit(player, Impl::EditType::Environment);
+    });
+    f.appendButton("成员权限"_trl(localeCode), "textures/ui/permissions_member_star", "path", [impl](Player& player) {
+        impl->sendEdit(player, Impl::EditType::Member);
+    });
+    f.appendButton("游客权限"_trl(localeCode), "textures/ui/permissions_visitor_hand", "path", [impl](Player& player) {
+        impl->sendEdit(player, Impl::EditType::Guest);
+    });
+    if (backTo) {
+        back_utils::injectBackButton(f, std::move(backTo));
     }
-
-    fm.sendTo(
-        player,
-        [cb = std::move(callback), json = std::move(json)](Player& pl, ll::form::CustomFormResult const& res, auto) {
-            if (!res) {
-                return;
-            }
-
-            auto copy = json;
-            for (auto const& [key, value] : copy.items()) {
-                bool const val = std::get<uint64_t>(res->at(key));
-                copy[key]      = val;
-            }
-
-            LandPermTable obj{};
-            json_util::json2struct(copy, obj);
-
-            cb(pl, obj);
-        }
-    );
+    f.sendTo(player);
 }
 
 } // namespace gui
